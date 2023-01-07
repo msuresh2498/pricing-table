5a9e2e6a952275b20bba7715a320f3944c9478ec
sureshm <msuresh2498@gmail.com>
2023-01-07 10:24:16 +0530

    finish

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <div className='pricing'>
       <Card className='Card'>
       <Card.Body>
-        <Card.Title className='text-muted'>FREE</Card.Title>
+        <Card.Title className='titletext text-muted'>FREE</Card.Title>
         <h3 className="pricers">$0<small className='month'>/month</small></h3>
         <Card.Text className='cardtext'><BiCheck />Single User</Card.Text>
         <Card.Text className='cardtext'><BiCheck />5GB Storage</Card.Text>
@@ -27,31 +27,31 @@ function App() {
     </Card>
     <Card className='Card'>
       <Card.Body>
-        <Card.Title className='text-muted'>FREE</Card.Title>
-        <h3 className="pricers">$0<small className='month'>/month</small></h3>
-        <Card.Text className='cardtext'><BiCheck />Single User</Card.Text>
-        <Card.Text className='cardtext'><BiCheck />5GB Storage</Card.Text>
+        <Card.Title className='titletext text-muted'>PLUS</Card.Title>
+        <h3 className="pricers">$9<small className='month'>/month</small></h3>
+        <Card.Text className='cardtext'><BiCheck /><strong>5 User</strong></Card.Text>
+        <Card.Text className='cardtext'><BiCheck />50GB Storage</Card.Text>
         <Card.Text className='cardtext'><BiCheck />Unlimited Public Projects</Card.Text>
         <Card.Text className='cardtext'><BiCheck />Community Access</Card.Text>
-        <Card.Text className='cardtext  text-muted'><FiX />Unlimited Private Projects</Card.Text>
-        <Card.Text className='cardtext  text-muted'><FiX />Dedicated Phone support</Card.Text>
-        <Card.Text className='cardtext  text-muted'><FiX />Free subdomain</Card.Text>
+        <Card.Text className='cardtext'><BiCheck />Unlimited Private Projects</Card.Text>
+        <Card.Text className='cardtext'><BiCheck />Dedicated Phone support</Card.Text>
+        <Card.Text className='cardtext'><BiCheck />Free subdomain</Card.Text>
         <Card.Text className='cardtext  text-muted'><FiX />Monthly Status Report</Card.Text>
         <Button className='button'>Buy</Button>
       </Card.Body>
     </Card>
     <Card className='Card'>
       <Card.Body>
-        <Card.Title className='text-muted'>FREE</Card.Title>
-        <h3 className="pricers">$0<small className='month'>/month</small></h3>
-        <Card.Text className='cardtext'><BiCheck />Single User</Card.Text>
-        <Card.Text className='cardtext'><BiCheck />5GB Storage</Card.Text>
+        <Card.Title className='titletext text-muted'>PRO</Card.Title>
+        <h3 className="pricers">$49<small className='month'>/month</small></h3>
+        <Card.Text className='cardtext'><BiCheck /><strong>Unlimited User</strong></Card.Text>
+        <Card.Text className='cardtext'><BiCheck />150GB Storage</Card.Text>
         <Card.Text className='cardtext'><BiCheck />Unlimited Public Projects</Card.Text>
         <Card.Text className='cardtext'><BiCheck />Community Access</Card.Text>
-        <Card.Text className='cardtext  text-muted'><FiX />Unlimited Private Projects</Card.Text>
-        <Card.Text className='cardtext  text-muted'><FiX />Dedicated Phone support</Card.Text>
-        <Card.Text className='cardtext  text-muted'><FiX />Free subdomain</Card.Text>
-        <Card.Text className='cardtext  text-muted'><FiX />Monthly Status Report</Card.Text>
+        <Card.Text className='cardtext'><BiCheck />Unlimited Private Projects</Card.Text>
+        <Card.Text className='cardtext'><BiCheck />Dedicated Phone support</Card.Text>
+        <Card.Text className='cardtext'><BiCheck /><strong>unlimited</strong> Free subdomain</Card.Text>
+        <Card.Text className='cardtext'><BiCheck />Monthly Status Report</Card.Text>
         <Button className='button'>Buy</Button>
       </Card.Body>
     </Card>
